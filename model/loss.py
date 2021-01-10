1ca220fefc28f065676493cbed226cf000415e59
Πατριτσια Παπαδατου <patritsiapapadatou@Patritsias-MacBook-Pro.local>
2021-01-10 22:47:40 +0100

    model.py/loss.py init

--- model/loss.py
+++ model/loss.py
@@ -1,5 +1,17 @@
-import torch.nn.functional as F
+import torch
+import torch.nn as nn
 
+def KLD(enc_mean, enc_log_std, prior_mean, prior_std):
+    
+    prior_log_std = torch.log(prior_std)
 
-def nll_loss(output, target):
-    return F.nll_loss(output, target)
+    enc_var   = torch.square(torch.exp(enc_log_std))
+    prior_var = torch.square(prior_std)
+    
+    mean_sq = torch.square(enc_mean - prior_mean)
+
+    KLD = prior_log_std - enc_log_std + (enc_var + mean_sq) 
+          / 2 * prior_var - 1 / 2
+
+    return KLD
+
--- model/model.py
+++ model/model.py
@@ -3,20 +3,77 @@ import torch.nn.functional as F
 from base import BaseModel
 
 
-class MnistModel(BaseModel):
-    def __init__(self, num_classes=10):
-        super().__init__()
-        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
-        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
-        self.conv2_drop = nn.Dropout2d()
-        self.fc1 = nn.Linear(320, 50)
-        self.fc2 = nn.Linear(50, num_classes)
+class Tabular_ModelEncoder(nn.Module):
+    
+    def __init__(self, input_dim, hidden_dims, z_dim):
 
+        super(Tabular_ModelEncoder,self).__init__()
+        
+        #Shared encoding layers of the model
+        self.shared_model = nn.Linear(input_dim, hidden_dims)
+        
+        #Output layers for each encoder
+        self.mean_encoder_1 = nn.Linear(hidden_dims, z_dim)
+        self.log_std_1      = nn.Linear(hidden_dims, z_dim)
+
+        self.mean_encoder_2 = nn.Linear(hidden_dims, z_dim)
+        self.log_std_2      = nn.Linear(hidden_dims, z_dim)
+
+        #Activation function
+        '(?)Not sure if in the experiments this act function is used'
+        self.act_f = nn.ReLU() 
+    
     def forward(self, x):
-        x = F.relu(F.max_pool2d(self.conv1(x), 2))
-        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
-        x = x.view(-1, 320)
-        x = F.relu(self.fc1(x))
-        x = F.dropout(x, training=self.training)
-        x = self.fc2(x)
-        return F.log_softmax(x, dim=1)
+        
+        #Output of shared layers followed by the activation
+        out_shared = self.act_f(self.shared_model(x))
+        
+        #Forward of each encoder
+        mean_t = self.mean_encoder_1(out_shared)
+        log_std_t = self.log_std_1(out_shared)
+
+        mean_s = self.mean_encoder_2(out_shared)
+        log_std_s = self.log_std_2(out_shared)
+
+        return mean_t, mean_s, log_std_t, log_std_s
+
+class Tabular_ModelDecoder(nn.Module):
+    
+    def __init__(self, z_dim, hidden_dims, target_classes, sensitive_classes):
+
+        super.init(Tabular_ModelDecoder, self).__init__()
+
+        #List of layers excluding the output layer
+        self.num_layers = [self.z_dim] + self.hidden_dims
+        
+        #Activation function
+        self.act_f = nn.ReLU()
+
+        #Architecture of the decoder
+        self.layers = []
+        
+        for layer_index in range(1, len(self.num_layers)):
+            self.layers += [nn.Linear(self.num_layers[layer_index - 1],
+                            self.num_layers[layer_index]), self.act_f]
+        
+        #Output layer
+        self.output_1 = nn.Linear(self.num_layers[-1], target_classes)
+        self.output_2 = nn.Linear(self.num_layers[-1], sensitive_classes)
+
+        self.Decoder = nn.ModuleList(self.layers)
+    
+    def forward(self, z_1, z_2):
+
+        for layers in self.Decoder:
+            out_1= layers(z_1)
+            out_2= layers(z_2)
+
+            x_1 = self.output_1(out_1)
+            x_2 = self.output_2(out_2)
+
+        return x_1, x_2
+
+
+
+
+
