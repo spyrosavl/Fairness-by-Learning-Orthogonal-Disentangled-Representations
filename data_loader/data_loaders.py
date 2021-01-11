c20962fe95e12cfb77e85651ad66dc78a995a822
danaiksez <dxezonaki@gmail.com>
2021-01-11 17:26:39 +0200

    one-hoted german dataloader

--- data_loader/data_loaders.py
+++ data_loader/data_loaders.py
@@ -1,6 +1,8 @@
+import numpy as np
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader, Dataset
 from base import BaseDataLoader
+from sklearn.preprocessing import MultiLabelBinarizer
 
 
 class CIFAR100DataLoader(BaseDataLoader):
@@ -37,33 +39,59 @@ class MnistDataLoader(BaseDataLoader):
         super().__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
 
 
-class GermanCreditDataset(Dataset):
-    def __init__(self, csv_file, root_dir=None, text_transforms=None):
-        self.csv_file = csv_file
+class GermanCreditDatasetOneHot(Dataset):
+    def __init__(self, txt_file, root_dir=None, text_transforms=None):
+        self.txt_file = txt_file
         self.root_dir = root_dir
         self.text_transforms = text_transforms
-        self.rows, self.targets, self.sensitive = self.get_data(self.csv_file)
+        self.categorical_columns = [0, 2, 3, 5, 6, 8, 9, 11, 13, 14, 16, 18, 19]
+        self.rows, self.targets, self.sensitive = self.get_data(self.txt_file)
+        self.features = self.get_onehot_attributes(self.rows, self.categorical_columns)
 
     def get_data(self, _file):
         rows=[];  targets=[]; sensitive=[]
-        with open(_file) as csv_file:
-          lines = csv_file.readlines()
+        with open(_file) as txt_file:
+          lines = txt_file.readlines()
           for l in lines:
             r = l.split(",")[0].split()
-            r = [int(i) for i in r]
             rows.append(r[:-1])
             targets.append(r[-1])
             sensitive.append(r[8])
+
+        cat = np.unique(sensitive)
+        cat = list(set(cat))
+        cat.sort()
+        one_hot = MultiLabelBinarizer(classes=cat).fit(cat)
+        sensitive = one_hot.transform(sensitive)
         return rows, targets, sensitive
+
+    def get_onehot_attributes(self, rows, columns):
+      rows = np.asarray(rows)
+      features = None
+      for i in range(len(rows[0])):
+        if i in columns:
+          occ = rows[:,i]
+          cat = np.unique(occ)
+          cat = list(set(cat))
+          cat.sort()
+          one_hot = MultiLabelBinarizer(classes=cat).fit(cat)
+          transformed = one_hot.transform(occ)
+          if features is not None:
+            features = np.column_stack((features, transformed))
+          else:
+            features = transformed
+        else:
+          features = np.column_stack((features, rows[:,i,None]))
+      features = np.asarray([[int(i) for i in j] for j in features])        
+      return features
 
     def __len__(self):
         return len(self.rows)
 
     def __getitem__(self, idx):
-        preprocessed_data = self.rows[idx]
+        preprocessed_data = self.features[idx]
         if self.text_transforms is not None:
             preprocessed_data = self.text_transforms(preprocessed_data)
-
         label = 0 if self.targets[idx] is 2 else 1
         sensitive = self.sensitive[idx]
         return preprocessed_data, sensitive, label
