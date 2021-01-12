fd15ed69826f38c2235438610290dd8e478e4221
danaiksez <dxezonaki@gmail.com>
2021-01-12 17:55:51 +0200

    adult dataloader one-hotted

--- data_loader/data_loaders.py
+++ data_loader/data_loaders.py
@@ -97,6 +97,70 @@ class GermanCreditDatasetOneHot(Dataset):
         return preprocessed_data, sensitive, label
 
 
+class AdultDatasetOneHot(Dataset):
+    def __init__(self, txt_file, root_dir=None, text_transforms=None):
+        self.txt_file = txt_file
+        self.root_dir = root_dir
+        self.text_transforms = text_transforms
+        self.categorical_columns = [1, 3, 5, 6, 7, 8, 9, 13]
+        self.rows, self.targets, self.sensitive = self.get_data(self.txt_file)
+        self.features = self.get_onehot_attributes(self.rows, self.categorical_columns)
+
+    def get_data(self, _file):
+        rows=[];  targets=[]; sensitive=[]
+        with open(_file) as txt_file:
+          lines = txt_file.readlines()
+          for l in lines:
+            r = l.split(",")
+            rows.append(r[:-1])
+            if len(r) > 1:
+              targets.append(r[-1])
+              sensitive.append(r[9])
+        cat = np.unique(sensitive)
+        cat = list(set(cat))
+        cat.sort()
+        one_hot = MultiLabelBinarizer(classes=cat).fit(cat)
+        sensitive = one_hot.transform(sensitive)
+        return rows, targets, sensitive
+
+    def get_onehot_attributes(self, rows, columns):
+      rows = np.asarray(rows)
+      features = None
+      for i in range(len(rows[0])):
+        if i in columns:
+          occ = rows[:,i]
+          cat = np.unique(occ)
+          cat = list(set(cat))
+          cat.sort()
+          one_hot = MultiLabelBinarizer(classes=cat).fit(cat)
+          transformed = one_hot.transform(occ)
+          if features is not None:
+            features = np.column_stack((features, transformed))
+          else:
+            features = transformed
+        else:
+          try:
+            if features is not None:
+              features = np.column_stack((features, rows[:,i,None]))
+            else:
+              features = rows[:,i,None]
+          except:
+            import pdb; pdb.set_trace()
+      features = np.asarray([[int(i) for i in j] for j in features])        
+      return features
+
+    def __len__(self):
+        return len(self.rows)
+
+    def __getitem__(self, idx):
+        preprocessed_data = self.features[idx]
+        if self.text_transforms is not None:
+            preprocessed_data = self.text_transforms(preprocessed_data)
+        label = 0 if self.targets[idx] is 2 else 1
+        sensitive = self.sensitive[idx]
+        return preprocessed_data, sensitive, label
+
+
 if __name__ == '__main__':
     german_dataset = GermanCreditDataset('./Downloads/GermanCreditDataset/german.data-numeric')
     german_dataloader = DataLoader(german_dataset, batch_size=16)
