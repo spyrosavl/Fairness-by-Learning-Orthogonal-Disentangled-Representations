df84f271ad54d2b16dffb6369bc3a024ba8cd26c
simontkl <72913950+simontkl@users.noreply.github.com>
2021-01-14 16:58:09 +0100

    Update util.py

--- utils/util.py
+++ utils/util.py
@@ -4,6 +4,8 @@ import pandas as pd
 from pathlib import Path
 from itertools import repeat
 from collections import OrderedDict
+from model.model import *
+from model.loss import *
 
 
 def ensure_dir(dirname):
