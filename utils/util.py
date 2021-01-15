31032f94ef71cd97ede93e5653bc24380665a963
danaiksez <dxezonaki@gmail.com>
2021-01-15 18:43:15 +0200

    loss kld fix

--- utils/util.py
+++ utils/util.py
@@ -9,6 +9,7 @@ from itertools import repeat
 from collections import OrderedDict
 from model.model import *
 from model.loss import *
+from torch.distributions.multivariate_normal import MultivariateNormal
 
 
 def ensure_dir(dirname):
@@ -82,26 +83,45 @@ class Criterion(nn.Module):
         self.step_size = step_size
 
         self.bce = nn.BCEWithLogitsLoss()
-        self.kld = nn.KLDivLoss(size_average=False)
+        self.kld = nn.KLDivLoss(reduction='batchmean')
         #TODO tensors through which we have to back propagate have to have require_grad = True (and be of type Parameter?)
 
     def forward(self, inputs, target, sensitive, current_step):
         mean_t, mean_s, log_std_t, log_std_s = inputs[0]
         y_zt, s_zt, s_zs = inputs[1]
+        z1, z2 = inputs[2]
 
         L_t = self.bce(y_zt, target[:,None].float())
         L_s = self.bce(s_zt, sensitive.float())
-#        Loss_e = L_e(s_zs)
         uniform = torch.rand(size=s_zs.size())
         Loss_e = self.kld(s_zs, uniform)
 
-        prior_mean_t = torch.from_numpy(np.array([0,1]).T)
-        prior_cov_t = torch.eye(2)
-        prior_mean_s = torch.from_numpy(np.array([1,0]).T)
-        prior_cov_s = torch.eye(2)
+#        prior_mean_t = torch.from_numpy(np.array([0,1]).T)
+#        prior_cov_t = torch.eye(2)
+#        prior_mean_s = torch.from_numpy(np.array([1,0]).T)
+#        prior_cov_s = torch.eye(2)
+
+        m_t = MultivariateNormal(torch.tensor([0.,1.]), torch.eye(2))
+        m_s = MultivariateNormal(torch.tensor([1.,0.]), torch.eye(2))
+
+        #TODO should the priors be the same for each iteration of the function?
+        # --> should we define them in init?        
         
-        L_zt = KLD(mean_t, log_std_t, prior_mean_t, prior_cov_t)
-        L_zs = KLD(mean_s, log_std_s, prior_mean_s, prior_cov_s)
+        prior_t=[]; prior_s=[]
+        for i in range(z1.shape[0]):
+            prior_t.append(m_t.sample())
+            prior_s.append(m_s.sample())
+
+        prior_t = torch.stack(prior_t)
+        prior_s = torch.stack(prior_s)
+
+        #L_zt = KLD(mean_t, log_std_t, prior_mean_t, prior_cov_t)
+        #L_zs = KLD(mean_s, log_std_s, prior_mean_s, prior_cov_s)
+
+        L_zt = self.kld(z1, prior_t)
+        L_zs = self.kld(z2, prior_s)
+
+
 
         lambda_e = self.lambda_e * self.gamma_e ** (current_step/self.step_size)
         lambda_od = self.lambda_od * self.gamma_od ** (current_step/self.step_size)
